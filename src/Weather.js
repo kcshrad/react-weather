30b715fbbc187861d9b1ad30852b53ba5097568d
kcshrad <kcshrad@gmail.com>
2022-02-19 23:33:52 +1100

    updates Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React from "react";
-import axios from "axios";
 import "./Weather.css";
 import "./App.css";
+import axios from "axios";
 
 export default function Weather() {
   let weatherData = {
